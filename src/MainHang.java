8b1b4ff940b314b512436603c09a2493c3c5b217
24HourDeveloper <grownmanherbal@gmail.com>
2017-11-21 10:11:06 -0500

    master branch redone with AQ code

--- src/MainHang.java
+++ src/MainHang.java
@@ -6,45 +6,59 @@ public class MainHang {
 	 * You get one letter at a time
 	 * game logic checks to see if the letter selected was correct
 	 */
-	
-	
+
 	public static void main(String[] args) {
 		//scanner so user can enter guess
 		Scanner input = new Scanner(System.in);
-		
-		// object that holds the hang man word
-		SecretWord secretWord = new SecretWord("Hangman");
-		String word = secretWord.getHangWord();
-		String emptyString = "";
-		
+
+		Words words = new Words();
+		words.pullWordsFromFile();
+
+		int random = (int) Math.round(Math.random() * (words.getList().size() - 1));
+
+		String secretWord = words.getList().get(random);
+
+		//provide the hint
+		System.out.println("Hint: " + words.getDescription().get(random));
+
+		StringBuilder emptyString = new StringBuilder("");
+		for (int c = 0; c < secretWord.length(); c++){
+			emptyString = emptyString.append("_");
+		}
+
 		int i = 0;
-		while(i < word.length()){
-		System.out.println("Enter A Letter: ");
-		String guess = input.nextLine();
-		
-		for(int x= 0; x < word.length(); x++){
-			if(Character.toString(word.charAt(x)).equals(guess)){
-				System.out.println(guess);
-				emptyString += guess;
-				System.out.println("Empty String "+emptyString);
-				break;
-			}else{
-				System.out.println("Letter not found. Try again!");
-				continue;
+		while (i < 6) {
+			System.out.println("Enter A Letter: ");
+			String guess = input.nextLine().toLowerCase();
+
+			if (guess.length() != 1){
+				System.out.println("invalid input");
+				return;
 			}
-			
+
+			int z = 0;
+			for (int a = 0; a < secretWord.length(); a++){
+				//placing the characters in the right spot
+				if (secretWord.toLowerCase().charAt(a) == guess.charAt(0)){
+					emptyString.setCharAt(a, guess.charAt(0));
+					z++;
+				}
+			}
+			//making sure that if the guess occurs more than one time it is only decremented one time
+			if (z == 1)
+				i--;
+			if (z > 1)
+				i--;
+
+			System.out.println(emptyString);
+
+			if (secretWord.equalsIgnoreCase(emptyString.toString())){
+				System.out.println("Congrats you won the game!!!!");
+				return;
+			}
+			i++;
 		}
-		}
-		
-		//System.out.println(word.charAt(0));
-		
-		/*if(guess.equals(word)){
-			System.out.println("Correct!!!");
-		}else{
-			System.out.println("Incorrect!!");
-		}*/
-		
-		//System.out.println(sw.getHangWord());
+		System.out.println("Sorry you lost the game. please try again!");
 	}
 
 }
---
+++ src/Words.java
@@ -0,0 +1,55 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by A on 11/18/2017.
+ */
+public class Words {
+
+    private List<String> list;
+    private String word;
+    private List<String> description;
+    private String hint;
+
+    public List<String> getList() {
+        return list;
+    }
+
+    public String getWord() {
+        return word;
+    }
+
+    public List<String> getDescription() {
+        return description;
+    }
+
+    public String getHint() {
+        return hint;
+    }
+
+    public void pullWordsFromFile() {
+
+        list = new ArrayList<>();
+        description = new ArrayList<>();
+        try {
+
+            BufferedReader br = new BufferedReader(new FileReader("HangmanWords"));
+            BufferedReader br2 = new BufferedReader(new FileReader("Description"));
+
+            while ((word = br.readLine()) != null) {
+                list.add(word);
+            }
+            while ((hint = br2.readLine()) != null){
+                description.add(hint);
+            }
+            br.close();
+            br2.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
